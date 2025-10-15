dcb6b585db9aa651457609b4e3fab9c513f514f3
Jye-Ming Serres <jyeming1001@gmail.com>
2025-10-15 18:32:46 -0400

    chore: use pyInstaller to package the project into an executable

---
+++ make.bat
@@ -0,0 +1,25 @@
+:: Easily configure pyInstaller options to package the project into an executable
+@ECHO OFF
+
+
+:: Name of the output executable file
+SET execname=Projection-of-3D-shapes
+
+:: Main script path
+SET mainscript=src/main.py
+
+:: Where the executable is stored
+SET distpath=./dist
+
+:: Where temporary files are stored
+SET workpath=./build
+
+
+:: call pyInstaller
+pyInstaller %mainscript% --name %execname% --onefile --noconsole --distpath %distpath% --workpath %workpath% --specpath %workpath%
+ECHO.
+IF %ERRORLEVEL% NEQ 0 (
+    ECHO pyInstaller failed to package the project.
+) ELSE (
+    ECHO Executable packaged successfully!
+)
